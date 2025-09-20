1ab08f445df1382f70783a6dc9d519d58b205027
superkingsely <superkingsely@gmail.com>
2025-09-20 20:08:23 +0100

    sqlite installed

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<UserService>();
+
+        return services;
+    }
+}
